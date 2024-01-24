263955ba76ce4cd5ea6145178c987e4884103061
Murat Simsek <muratsimseekk@gmail.com>
2024-01-25 00:17:31 +0300

    Solves first part of the homework

---
+++ src/Employee.java
@@ -0,0 +1,49 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class Employee {
+    private long id ;
+    private String firstName;
+    private String lastName;
+
+    public Employee(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee employee)) return false;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        LinkedList <Employee> employeeList = new LinkedList<>();
+
+        employeeList.add(new Employee(1,"Murat","Simsek"));
+        employeeList.add(new Employee(2, "Ali","Veli"));
+        employeeList.add(new Employee(3, "Mustafa","Kemal"));
+        employeeList.add(new Employee(3, "Fevzi","Cakmak"));
+        employeeList.add(new Employee(5, "Veli","Ali"));
+
+
+
+
+        LinkedList <Employee> duplicatedList = new LinkedList<>();
+        Map<Long,Employee> uniqueList = new HashMap<>();
+
+        for (Employee employee : employeeList){
+            if (uniqueList.containsKey(employee.getId())){
+                duplicatedList.add(employee);
+            }
+            else {
+                uniqueList.put(employee.getId(),employee);
+            }
+        }
+
+        employeeList.removeAll(duplicatedList);
+
+
+        printEmployeeList(employeeList);
+        printUniqueEmployeeList(uniqueList);
+    }
+    private static void printEmployeeList (LinkedList<Employee> list){
+        for (Employee employee : list){
+            System.out.println(employee.getId() + " id li employee :" + employee.getFirstName() + " " + employee.getLastName());
+        }
+    }
+    private static void printUniqueEmployeeList (Map <Long , Employee> map ){
+        for (Map.Entry<Long , Employee> entry : map.entrySet()){
+            Employee employee = entry.getValue();
+            System.out.println("Unique Employee List : " +employee.getId() + " id li employee : " + employee.getFirstName() + " " + employee.getLastName());
+        }
     }
 }
